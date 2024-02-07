8d9bb579680d77fcc0e30119249ffdb49d29efd0
Abdur Rakib <rakib15-13656@diu.edu.bd>
2024-02-07 23:40:14 +0600

    create necessary model for the project

--- user/serializers/registration_serializer.py
+++ user/serializers/registration_serializer.py
@@ -5,7 +5,14 @@ class RegistrationSerializer(serializers.ModelSerializer):
     password2 = serializers.CharField(style={'input_type':'password'}, write_only=True)
     class Meta:
         model = User
-        fields = ['email', 'username', 'password', 'password2']
+        fields = [
+            'email', 
+            'username',
+            'first_name',
+            'last_name',
+            'password', 
+            'password2'
+        ]
         extra_kwargs = {
             'password':{'write_only':True}
         }
